e2debf7d630b961ffbf15fefce946871bdcbe7bc
Cauê Amaral <caue.amaral@hotmail.com>
2026-08-17 15:11:41 -0300

    style: center header content in a fixed-width bar

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,15 +1,34 @@
+import styled from "styled-components";
 import Image from "next/image";
+
+const HeaderContainer = styled.header`
+  padding-inline: 24px;
+`;
+
+const HeaderLimit = styled.div`
+  background: color-mix(in srgb, #151515 90%, transparent);
+  border-radius: 8px;
+  display: flex;
+  height: 76px;
+  margin-top: 24px;
+  margin-inline: auto;
+  max-width: 1280px;
+  padding-inline: 27px;
+  width: 100%;
+`;
 
 export default function Header() {
   return (
-    <header>
-      <Image
-        src="/images/logo-movizone.svg"
-        alt="MoviZone logo"
-        width={154}
-        height={22}
-        priority
-      />
-    </header>
+    <HeaderContainer>
+      <HeaderLimit>
+        <Image
+          src="/images/logo-movizone.svg"
+          alt="MoviZone logo"
+          width={154}
+          height={22}
+          priority
+        />
+      </HeaderLimit>
+    </HeaderContainer>
   );
 }
