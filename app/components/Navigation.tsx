b30e8f232f8c3c38bd222889318b594abd658546
Cauê Amaral <caue.amaral@hotmail.com>
2026-08-18 14:48:42 -0300

    feat: add the header navigation links

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -1,3 +1,40 @@
+import styled from "styled-components";
+import Link from "next/link";
+
+const List = styled.ul`
+  display: flex;
+  gap: 30px;
+`;
+
+const NavLink = styled(Link)`
+  &:hover {
+    color: var(--red);
+  }
+`;
+
 export default function Navigation() {
-  return <nav>Nav</nav>;
+  return (
+    <nav>
+      <List>
+        <li>
+          <NavLink href="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink href="/movies">Movies</NavLink>
+        </li>
+        <li>
+          <NavLink href="/tv-shows">TV Shows</NavLink>
+        </li>
+        <li>
+          <NavLink href="anime">Anime</NavLink>
+        </li>
+        <li>
+          <NavLink href="series">Series</NavLink>
+        </li>
+        <li>
+          <NavLink href="trending">Trending</NavLink>
+        </li>
+      </List>
+    </nav>
+  );
 }
