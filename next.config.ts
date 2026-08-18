1fa9f8ae5bb1d2d40a42ff3cabceef20a3b8aa7d
Cauê Amaral <caue.amaral@hotmail.com>
2026-08-18 08:53:59 -0300

    build: drop the unused picsum.photos image pattern

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [new URL("https://picsum.photos/**")],
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
