2ae30995be36117da3fb7926f2521f955fc49fe1
Cauê Amaral <caue.amaral@hotmail.com>
2026-08-24 10:26:10 -0300

    feat: add the Bebas Neue font

--- app/fonts/googleFonts.ts
+++ app/fonts/googleFonts.ts
@@ -1,4 +1,9 @@
-import { Montserrat } from "next/font/google";
+import { Bebas_Neue, Montserrat } from "next/font/google";
+
+export const bebas_neue = Bebas_Neue({
+  subsets: ["latin"],
+  weight: "400",
+});
 
 export const montserrat = Montserrat({
   subsets: ["latin"],
