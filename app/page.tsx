a8e1a58f9dda97e366406bcfffcc900dcca1bace
Cauê Amaral <caue.amaral@hotmail.com>
2026-08-25 17:33:16 -0300

    style: cap the page width at 1440px

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,21 @@
+import styled from "styled-components";
 import Header from "@/app/components/header/Header";
 import Banner from "@/app/components/banner/Banner";
 
+const Container = styled.div`
+  @media (min-width: 1100px) {
+    margin-inline: auto;
+    max-width: 1440px;
+  }
+`;
+
 export default function Home() {
   return (
-    <>
+    <Container>
       <Header />
       <main>
         <Banner />
       </main>
-    </>
+    </Container>
   );
 }
