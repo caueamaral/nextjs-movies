37ceea502b86c7668509cf0fc987d73dc4d296d2
Cauê Amaral <caue.amaral@hotmail.com>
2026-08-31 08:34:51 -0300

    refactor: drop redundant sizing from the banner image

--- app/components/banner/Banner.tsx
+++ app/components/banner/Banner.tsx
@@ -40,13 +40,10 @@ const Container = styled.section`
 `;
 
 const BannerImage = styled(Image)`
-  height: 100%;
   inset: 0;
-  margin: auto;
   object-fit: cover;
   object-position: top;
   position: absolute;
-  width: 100%;
 `;
 
 const Limit = styled.div`
